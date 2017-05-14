563c55b5ccd8797ca1de58302321d22b03670c1b
Nelocanelo <nelocanelo@gmail.com>
2017-05-14 19:50:30 +0200

    Empezado PLanet Generator

---
+++ Planet Generator/Assets/Scripts/GeneratePlanet.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GeneratePlanet : MonoBehaviour {
+
+    public int seed;
+    public float scale;
+    public int octaves;
+    [Range(0,1)]
+    public float persistence;
+    public float lacunarity;
+
+    private Vector3[] vertices;
+    private Vector3[] normales;
+    private Mesh mesh;
+
+	void Start () {
+
+        mesh = GetComponent<MeshFilter>().mesh;
+        vertices = mesh.vertices;
+        normales = mesh.normals;
+
+        calculateHeight();
+
+    }
+	
+	
+	void calculateHeight () {
+
+        System.Random prng = new System.Random(seed);
+        Vector2[] octaveOffsets = new Vector2[octaves];
+        for (int i = 0; i < octaves; i++)
+        {
+
+        }
+
+    }
+}
---
+++ Planet Generator/Assets/Scripts/SphereDeform.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SphereDeform : MonoBehaviour {
+
+    private Mesh mesh;
+
+	void Start () {
+        mesh = GetComponent<MeshFilter>().mesh;
+	}
+	
+
+	void Update () {
+		
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            Expand();
+        }else if (Input.GetKeyDown(KeyCode.Return))
+        {
+            Recolor();
+        }
+	}
+
+    void Expand()
+    {
+        Vector3[] nuevosVertices = mesh.vertices;
+        Vector3[] normales = mesh.normals;
+
+        for (int i = 0; i < nuevosVertices.Length; i++)
+        {
+            nuevosVertices[i] += normales[i] * 2;
+        }
+
+        mesh.vertices = nuevosVertices;
+    }
+
+    void Recolor()
+    {
+        Debug.Log("A pintar!");
+        Vector3[] vertices = mesh.vertices;
+        Color[] colores = new Color[vertices.Length];
+
+        for (int i = 0; i < colores.Length; i++)
+        {
+            colores[i] = Color.blue;
+        }
+
+        mesh.colors = colores;
+    }
+}
